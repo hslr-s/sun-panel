4df58fec7b2054ce97cf2989045affd144aa4f8b
Sun <95302870@qq.com>
2023-11-22 22:38:27 +0800

    完善关于页面

--- src/views/exception/test/index.vue
+++ src/views/exception/test/index.vue
@@ -1,24 +1,41 @@
 <script setup lang="ts">
-import { VueDraggable } from 'vue-draggable-plus'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import { NColorPicker } from 'naive-ui'
 
-const list = ref<[any]>([])
-for (let i = 1; i < 10; i++) {
-  list.value.push({
-    id: 1,
-    name: `哈哈哈哈${i}`,
-  })
+// 获取背景颜色的 RGB 值（假设这里获取到了背景颜色的值）
+const bgColor = ref('#000000') // 假设为蓝色
+
+// 计算颜色的明暗度
+const calculateLuminance = (color: string) => {
+  const hex = color.replace(/^#/, '')
+  const r = parseInt(hex.substring(0, 2), 16)
+  const g = parseInt(hex.substring(2, 4), 16)
+  const b = parseInt(hex.substring(4, 6), 16)
+  return (0.299 * r + 0.587 * g + 0.114 * b) / 255
 }
+
+// 根据明暗度切换文本颜色
+const textColor = computed(() => {
+  const luminance = calculateLuminance(bgColor.value)
+  return luminance > 0.5 ? 'black' : 'white'
+})
 </script>
 
 <template>
-  <div>
-    <!-- <div class="mt-4 grid lg:grid-cols-12 gap-5"> -->
-    <VueDraggable v-model="list" item-key="id" class="mt-4 grid grid-cols-3 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-10 xl:12 gap-5">
-      <div v-for="item, index in list" :key="index" class="bg-[pink] m-[10px]">
-        {{ item.name }}
-      </div>
-    </VueDraggable>
-    <!-- </div> -->
+  <div :style="{ backgroundColor: bgColor, color: textColor }">
+    Background Color Example
   </div>
+  <NColorPicker v-model:value="bgColor" />
 </template>
+
+<style scoped>
+/* 样式可以根据实际需求自定义 */
+div {
+  width: 200px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+}
+</style>
--- src/views/home/components/Setting/tabs/About.vue
+++ src/views/home/components/Setting/tabs/About.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { NDivider, NGradientText } from 'naive-ui'
+import { NDivider } from 'naive-ui'
 import { onMounted, ref } from 'vue'
 import { get } from '@/api/system/about'
+import srcSvglogo from '@/assets/logo.svg'
 
 interface Version {
   versionName: string
@@ -20,20 +21,32 @@ onMounted(() => {
 
 <template>
   <div>
-    <div class="text-3xl">
-      <NGradientText type="danger">
-        Sun-Panel
-      </NGradientText>
-      v{{ versionName }}
+    <div>
+      <div class="flex flex-col items-center justify-center">
+        <img :src="srcSvglogo" width="100" height="100" alt="">
+        <div class="text-3xl">
+          {{ $t('common.appName') }}
+        </div>
+        <div class="text-xl">
+          - v{{ versionName }} -
+        </div>
+      </div>
     </div>
     <NDivider />
-    <div class="text-lg">
-      开发者： <a href="https://blog.enianteam.com/u/sun/content/11" target="_blank" class="link">红烧猎人</a>
-    </div>
-    <div class="text-lg">
-      项目开源地址：
-      <a href="https://github.com/hslr-s/sun-panel" target="_blank" class="link">Github</a> |
-      <a href="https://gitee.com/hslr/sun-panel" target="_blank" class="link">Gitee</a>
+    <div class="flex flex-col items-center justify-center">
+      <div class="text-lg">
+        开发者： <a href="https://blog.enianteam.com/u/sun/content/11" target="_blank" class="link">红烧猎人</a>
+      </div>
+      <div class="text-lg">
+        QQ交流群：<a href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=_I9WIoJn1roIdoaAqelSj9qClLKlXIa1&authKey=GfsQP2GagHnus0jMc7U8Sm6VhWjtsipXUzCHbFwQsGyHMgmYWx6ZbAP%2Bhut%2B4D6N&noverify=0&group_code=276594668" target="_blank" class="link">276594668</a>
+      </div>
+      <div class="text-lg">
+        项目地址：
+        <a href="https://github.com/hslr-s/sun-panel" target="_blank" class="link">Github</a> |
+        <!-- <a href="https://gitee.com/hslr/sun-panel/stargazers"><img src="https://gitee.com/hslr/sun-panel/badge/star.svg?theme=dark" alt="star"></a> -->
+        <a href="https://gitee.com/hslr/sun-panel" target="_blank" class="link">Gitee</a> |
+        <a href="https://hub.docker.com/r/hslr/sun-panel" target="_blank" class="link">Docker</a>
+      </div>
     </div>
   </div>
 </template>
