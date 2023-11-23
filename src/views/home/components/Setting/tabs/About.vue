2ff2b6b32a4bb70653e3a7312ccb0f4b0b945f07
Sun <95302870@qq.com>
2023-11-23 23:45:10 +0800

    优化关于页面，及更新版本序号为2

--- src/views/home/components/Setting/tabs/About.vue
+++ src/views/home/components/Setting/tabs/About.vue
@@ -3,6 +3,9 @@ import { NDivider } from 'naive-ui'
 import { onMounted, ref } from 'vue'
 import { get } from '@/api/system/about'
 import srcSvglogo from '@/assets/logo.svg'
+import srcGitee from '@/assets/about_image/gitee.png'
+import srcGithub from '@/assets/about_image/github.png'
+import srcDocker from '@/assets/about_image/docker.png'
 
 interface Version {
   versionName: string
@@ -33,19 +36,28 @@ onMounted(() => {
       </div>
     </div>
     <NDivider />
-    <div class="flex flex-col items-center justify-center">
-      <div class="text-lg">
-        开发者： <a href="https://blog.enianteam.com/u/sun/content/11" target="_blank" class="link">红烧猎人</a>
-      </div>
-      <div class="text-lg">
+    <div class="flex flex-col items-center justify-center text-base">
+      <div>
         QQ交流群：<a href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=_I9WIoJn1roIdoaAqelSj9qClLKlXIa1&authKey=GfsQP2GagHnus0jMc7U8Sm6VhWjtsipXUzCHbFwQsGyHMgmYWx6ZbAP%2Bhut%2B4D6N&noverify=0&group_code=276594668" target="_blank" class="link">276594668</a>
       </div>
-      <div class="text-lg">
-        项目地址：
-        <a href="https://github.com/hslr-s/sun-panel" target="_blank" class="link">Github</a> |
-        <!-- <a href="https://gitee.com/hslr/sun-panel/stargazers"><img src="https://gitee.com/hslr/sun-panel/badge/star.svg?theme=dark" alt="star"></a> -->
-        <a href="https://gitee.com/hslr/sun-panel" target="_blank" class="link">Gitee</a> |
-        <a href="https://hub.docker.com/r/hslr/sun-panel" target="_blank" class="link">Docker</a>
+
+      <div>
+        开发者： <a href="https://blog.enianteam.com/u/sun/content/11" target="_blank" class="link">红烧猎人</a>
+      </div>
+
+      <div class="flex mt-[10px]">
+        <div class="flex items-center mx-[10px]">
+          <img class="w-[20px] h-[20px] mr-[5px]" :src="srcGithub" alt="">
+          <a href="https://github.com/hslr-s/sun-panel" target="_blank" class="link">Github</a>
+        </div>
+        <div class="flex items-center mx-[10px]">
+          <img class="w-[20px] h-[20px] mr-[5px]" :src="srcGitee" alt="">
+          <a href="https://gitee.com/hslr/sun-panel" target="_blank" class="link">Gitee</a>
+        </div>
+        <div class="flex items-center mx-[10px]">
+          <img class="w-[20px] h-[20px] mr-[5px]" :src="srcDocker" alt="">
+          <a href="https://hub.docker.com/r/hslr/sun-panel" target="_blank" class="link">Docker</a>
+        </div>
       </div>
     </div>
   </div>
