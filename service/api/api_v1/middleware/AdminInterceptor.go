393a80900a80097e99e569dbe35c0685a89b942f
Sun <95302870@qq.com>
2023-12-01 20:03:22 +0800

    取消登录调试

--- service/api/api_v1/middleware/AdminInterceptor.go
+++ service/api/api_v1/middleware/AdminInterceptor.go
@@ -6,14 +6,12 @@ import (
 
 	"sun-panel/api/api_v1/common/apiReturn"
 	"sun-panel/api/api_v1/common/base"
-	"sun-panel/global"
 
 	"github.com/gin-gonic/gin"
 )
 
 func AdminInterceptor(c *gin.Context) {
 	currentUser, _ := base.GetCurrentUserInfo(c)
-	global.Logger.Debugln("登录用户", currentUser)
 	if currentUser.Role != 1 {
 		apiReturn.ErrorNoAccess(c)
 		c.Abort()
