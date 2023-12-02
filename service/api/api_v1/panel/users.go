c6805c99f5decaf204e9d3958914338649c9e9e6
Sun <95302870@qq.com>
2023-12-02 22:26:08 +0800

    增加访客模式的获取和设置接口

--- service/api/api_v1/panel/users.go
+++ service/api/api_v1/panel/users.go
@@ -2,6 +2,7 @@ package panel
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 	"sun-panel/api/api_v1/common/apiReturn"
 	"sun-panel/api/api_v1/common/base"
@@ -242,4 +243,45 @@ func (a UsersApi) GetList(c *gin.Context) {
 	// }
 
 	apiReturn.SuccessListData(c, list, count)
+}
+
+func (a UsersApi) SetPublicVisitUser(c *gin.Context) {
+	type Req struct {
+		UserId *uint `json:"userId"`
+	}
+
+	req := Req{}
+	if err := c.ShouldBindBodyWith(&req, binding.JSON); err != nil {
+		apiReturn.ErrorParamFomat(c, err.Error())
+		return
+	}
+
+	if req.UserId != nil {
+		userInfo := models.User{}
+		if err := global.Db.First(&userInfo, "id=?", req.UserId).Error; err != nil {
+			fmt.Println(err, userInfo)
+			apiReturn.ErrorDataNotFound(c)
+			return
+		}
+	}
+
+	if err := global.SystemSetting.Set("publicUserId", req.UserId); err != nil {
+		apiReturn.Error(c, "set fail")
+		return
+	}
+	apiReturn.Success(c)
+}
+
+func (a UsersApi) GetPublicVisitUser(c *gin.Context) {
+	var userId *uint
+	if err := global.SystemSetting.GetValueByInterface("publicUserId", &userId); err == nil && userId != nil {
+		userInfo := models.User{}
+		if err := global.Db.First(&userInfo, "id=?", userId).Error; err == nil {
+			apiReturn.SuccessData(c, userInfo)
+			return
+		}
+	}
+
+	// 没有此配置
+	apiReturn.ErrorDataNotFound(c)
 }
