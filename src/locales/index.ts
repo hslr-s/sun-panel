f0e425737217083668a00212dba5f7e82cab9a02
Sun <95302870@qq.com>
2023-12-08 13:16:42 +0800

    删除暂时无用语言包

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,10 +1,10 @@
 import type { App } from 'vue'
 import { createI18n } from 'vue-i18n'
-import enUS from './en-US'
-import koKR from './ko-KR'
+// import enUS from './en-US'
+// import koKR from './ko-KR'
 import zhCN from './zh-CN'
-import zhTW from './zh-TW'
-import ruRU from './ru-RU'
+// import zhTW from './zh-TW'
+// import ruRU from './ru-RU'
 import { useAppStoreWithOut } from '@/store/modules/app'
 import type { Language } from '@/store/modules/app/helper'
 
@@ -17,11 +17,11 @@ const i18n = createI18n({
   fallbackLocale: 'en-US',
   allowComposition: true,
   messages: {
-    'en-US': enUS,
-    'ko-KR': koKR,
+    // 'en-US': enUS,
+    // 'ko-KR': koKR,
     'zh-CN': zhCN,
-    'zh-TW': zhTW,
-    'ru-RU': ruRU,
+    // 'zh-TW': zhTW,
+    // 'ru-RU': ruRU,
   },
 })
 
