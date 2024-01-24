e0dcc49f5b23618103c5ee15e13758164d2eddf2
Sun <95302870@qq.com>
2024-01-24 13:55:28 +0800

    优化部分错误码

--- service/api/api_v1/common/apiReturn/ErrorCode.go
+++ service/api/api_v1/common/apiReturn/ErrorCode.go
@@ -1,12 +1,15 @@
 package apiReturn
 
 var ErrorCodeMap = map[int]string{
-	// -1:操作失败
+	// -1:操作失败(前端会自动弹窗)
+	// 100: "operation failed",
+
 	1000: "Not logged in yet",                   // 还未登录
 	1003: "Incorrect username or password",      // 用户名或密码错误
 	1004: "Account disabled or not activated",   // 账号已停用或未激活
 	1005: "No current permission for operation", // 当前无权限操作
 	1006: "Account does not exist",              // 账号不存在
+	1007: "Old password error",                  // 旧密码不正确
 
 	// 数据类
 	1200: "Database error",           // 数据库错误
